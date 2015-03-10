9d60fda1c5f4a4db0c1723838d60020754eb1fdd
hdlopesrocha <hdlopesrocha91@gmail.com>
2015-03-10 17:29:52 +0000

    [ADD] added argv stuff

--- eadw/eadw02.py
+++ eadw/eadw02.py
@@ -1,13 +1,21 @@
 # -*- coding: UTF-8 -*-
+import sys
+
 from utils import InvertedIndex, Split, DocumentFrequency, \
     MinMaxDocumentFrequency, InverseDocumentFrequency, DotProduct, SortByValue
+
+
+args = sys.argv;
+args.pop(0)
+print "The arguments are: " , str(args)
+
 
 
 file1 = open("lab02_documents.txt","r")
 lines = file1.readlines()
 file1.close()
 index = InvertedIndex(lines,Split)
-df = DocumentFrequency(index,"with","the","lol","amorphous")
+df = DocumentFrequency(index,*args)
 
 print "Exercise 1"
 print index
@@ -26,14 +34,17 @@ print "# individual terms = ", len(index.keys())
 
 print "df = ", df
 
-print "min_max = ", MinMaxDocumentFrequency(index,"with","the","lol","amorphous")
+print "min_max = ", MinMaxDocumentFrequency(index,*args)
 
-print "idf = ", InverseDocumentFrequency(index,len(lines),"with")
+for arg in args:
+    print "idf("+arg+") = ", InverseDocumentFrequency(index,len(lines),arg)
 
 
 print "Exercise 3"
-dot = DotProduct(index,len(lines),"with","the","lol","amorphous")
+dot = DotProduct(index,len(lines),*args)
 print dot
 
 print "Exercise 4"
 print SortByValue(dot)
+
+
