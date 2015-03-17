f5344a3b2644a94222c1eda728417e1958f82908
hdlopesrocha <hdlopesrocha91@gmail.com>
2015-03-17 13:36:15 +0000

    [ADD] maybe 4 is done

--- eadw/eadw03.py
+++ eadw/eadw03.py
@@ -3,20 +3,24 @@ from whoosh.index import create_in
 from whoosh.index import open_dir
 from whoosh.qparser import *
 
-from utils_whoosh import search, precision, recall, f1
+from utils_whoosh import search, precision, recall, f1, score
 
 
 file1 = open("aula03_cfc.txt","r")
-lines = file1.readlines()
+lines1 = file1.readlines()
+
+file2 = open("aula03_queries.txt","r")
+lines2 = file2.readlines()
+
 
 print "Exercise 1"
 schema = Schema(id = NUMERIC(stored=True), content=TEXT)
 ix = create_in("indexdir", schema)
 writer = ix.writer()
-for line in lines:
+for line in lines1:
     
     partition = line.split(' ',1)
-    i = partition[0]
+    i = int(partition[0])
     c = partition[1]
     writer.add_document(id=i,content=unicode(c,"UTF-8"))
 writer.commit()
@@ -24,10 +28,20 @@ writer.commit()
 
 
 print "Exercise 2"
-result = search(ix,"first document")
+result = search(ix,"first document",100)
 print result
 
 print "Exercise 3"
-print "Pr = ", precision(result,['00664','00276','00001'])
-print "Re = ", recall(result,['00664','00276','00001'])
-print "F1 = ", f1(result,['00664','00276','00001'])
+print "Pr = ", precision(result,[664,276,1])
+print "Re = ", recall(result,[664,276,1])
+print "F1 = ", f1(result,[664,276,1])
+
+print "Exercise 4"
+for i in range(0,len(lines2)/2):
+    text = lines2[i*2+0];
+    number_strings = lines2[i*2+1].split();
+    expected = [int(el) for el in number_strings]
+    search2 = search(ix,text,100)
+    print score(search2,expected)
+
+
--- eadw/utils_whoosh.py
+++ eadw/utils_whoosh.py
@@ -3,14 +3,14 @@ from whoosh.qparser.default import QueryParser
 from whoosh.qparser.syntax import OrGroup
 
 
-def search(index,query):
+def search(index,query,lim):
     index = open_dir("indexdir")
     
     res = []
     
     with index.searcher() as searcher:
         query = QueryParser("content", index.schema, group=OrGroup).parse(unicode(query,"UTF-8"))
-        results = searcher.search(query, limit=100)
+        results = searcher.search(query, limit=lim)
         for r in results:
             res.append(r["id"])
         
@@ -31,3 +31,13 @@ def f1(result,expected):
     Pr=precision(result, expected)
     Re=recall(result, expected)
     return (2*Re*Pr)/(Re+Pr)
+
+
+def score(result,expected):
+    intersection = [val for val in result if val in expected]
+    if len(intersection)>0:
+        Pr=float(len(intersection))/len(result)
+        Re=float(len(intersection))/len(expected)
+        F1=(2*Re*Pr)/(Re+Pr)
+        return {"Pr":Pr,"Re":Re,"F1":F1}
+    return {"Pr":0,"Re":0,"F1":0}
